ae25b88395ca2cce1b1aa4944db3e39f0f668a30
Asad Ali <asadali.code123@gmail.com>
2024-07-29 15:28:09 +0500

    cleaned up spaces

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,49 @@
 import React, { Suspense } from 'react';
 import { IRoute } from "./@types/route";
-
-
 import './App.scss'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ShellContainer from './components/containers/shellContainer';
 const CreateNotification = React.lazy(() => import('./pages/notifications/createNotification'));
 const ViewNotification = React.lazy(() => import('./pages/notifications/viewNotification'));
 
-const App =()=> {
-
-const routes: IRoute[] = [
-{
-    link: "/",
-    element: <ShellContainer />,
-    children: [
-      {
-        link: "",
-        element: <CreateNotification />,
-      },
-      {
-        link: "view-notification",
-        element: <ViewNotification />,
-      }
-
-    ]
-},
-]
+const App = () => {
+  const routes: IRoute[] = [
+    {
+      link: "/",
+      element: <ShellContainer />,
+      children: [
+        {
+          link: "",
+          element: <CreateNotification />,
+        },
+        {
+          link: "view-notification",
+          element: <ViewNotification />,
+        }
+      ]
+    }]
 
   return (
     <BrowserRouter>
-    <Suspense fallback={<div>Loading...</div>}>
-    <Routes>
-      {routes.map(({ link, element, children }: IRoute) => (
-        <Route
-          key={link}
-          path={link}
-          element={element}
-        >
-         {children &&
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          {routes.map(({ link, element, children }: IRoute) => (
+            <Route
+              key={link}
+              path={link}
+              element={element}
+            >
+              {children &&
                 children.length > 0 &&
                 children.map(({ link, element }: IRoute) => (
                   <Route key={link} path={link} element={element} />
                 ))}
-        </Route>
-      ))}
-      {/* <Route path="*" element={<Navigate to="/not-found" />} /> */}
-    </Routes>
-    </Suspense>
-  </BrowserRouter>
+            </Route>
+          ))}
+          {/* <Route path="*" element={<Navigate to="/not-found" />} /> */}
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
   )
 }
 
